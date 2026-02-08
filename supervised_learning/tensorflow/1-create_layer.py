d0dacf11255891f84c1e25f9f7e5d8d1676190c5
Sheryl <s.otieno@alustudent.com>
2026-02-08 19:01:43 +0300

    Add create_layer function

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -22,3 +22,4 @@ def create_layer(prev, n, activation):
     init = tf.contrib.layers.variance_scaling_initializer(mode='FAN_AVG')
     layer = tf.layers.Dense(n, activation=activation, kernel_initializer=init)
     return layer(prev)
+    
